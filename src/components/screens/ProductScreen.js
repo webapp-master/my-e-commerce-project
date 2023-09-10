eb832cc39fa58edd4822d95168a43fe7414c5f83
webapp-master <ogundirantoluwani@gmail.com>
2023-09-10 22:27:19 +0100

    V1

--- src/components/screens/ProductScreen.js
+++ src/components/screens/ProductScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap';
-import products from '../../products';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import products from "../../products";
 
-import { useParams } from 'react-router-dom'; // Import useParams
+import { useParams } from "react-router-dom"; // Import useParams
+import Rating from "../Rating";
 
 function ProductScreen() {
   // Use useParams to get the 'id' parameter
   const { id } = useParams();
-  
+
   // Find the product using the 'id' parameter
   const product = products.find((p) => p._id === id);
 
@@ -19,7 +20,57 @@ function ProductScreen() {
 
   return (
     <div>
-      <h1>{product.name}</h1>
+      <Link to="/" className="btn btn-dark my-3">
+        {" "}
+        Go Back
+      </Link>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+                color={"#f8e825"}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? "In Stock" : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 }
